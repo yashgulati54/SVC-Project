0800cea0072e2c645d7d7e3c0dd2f34a8da5a880
Shivi Sharma <86220450+s-shr05@users.noreply.github.com>
2023-12-05 23:16:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,3 +21,27 @@ document.addEventListener("keypress", function(event) {
   buttonAnimation(event.key);
 
 });
+
+
+function makeSound(key) {
+
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
